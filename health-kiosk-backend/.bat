a84685e73360b4b20419ae97fd95a82781738ff5
liruize830 <lrz08302005@163.com>
2026-04-12 11:07:53 +0800

    delete self data

--- health-kiosk-backend/.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d D:\helath\health-kiosk\health-kiosk-backend
-call conda activate face_recognition
-node app.js
--- health-kiosk-frontend/.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d D:\helath\health-kiosk\health-kiosk-frontend
-npm run dev
